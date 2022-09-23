cb44f2703046cf6bb30e7f44a406f818c595df29
rah-emil <013131@mail.ru>
2022-09-24 00:06:49 +0300

    build: добавлен vitest + базовый тест

---
+++ tests/example.test.js
@@ -0,0 +1,8 @@
+import { describe, expect, test } from 'vitest';
+
+describe('My test', async () => {
+    test('my test', async () => {
+        const count = 1;
+        expect(count).toBe(1);
+    })
+})
---
+++ tests/mocks/example.mock.ts
@@ -0,0 +1,18 @@
+import { Example } from "~~/models/example.model";
+
+const exampleMocks: Example[] = [
+    {
+        id: 1,
+        name: 'First name'
+    },
+    {
+        id: 2,
+        name: 'Next name'
+    },
+    {
+        id: 3,
+        name: 'Latest name'
+    },
+]
+
+export default exampleMocks;
