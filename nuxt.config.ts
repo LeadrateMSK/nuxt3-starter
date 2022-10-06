14c37a891fba2eb2310dcf5d7921366e2e1cf6cc
rah-emil <013131@mail.ru>
2022-10-06 20:21:46 +0400

    build: корректировки общих файлов для сборки

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,22 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [
-        [
-          '@pinia/nuxt',
-          {
-            autoImports: [
-              'defineStore',
-              ['defineStore', 'definePiniaStore'],
-            ],
-          },
+  runtimeConfig: {
+    apiSecret: '1234567890',
+    public: {
+      apiBase: '',
+    }
+  },
+
+  modules: [
+    // Pinia: https://pinia.vuejs.org/ssr/nuxt.html
+    [
+      '@pinia/nuxt',
+      {
+        autoImports: [
+          'defineStore',
+          ['defineStore', 'definePiniaStore'],
         ],
-      ],
-})
+      },
+    ],
+  ],
+});
